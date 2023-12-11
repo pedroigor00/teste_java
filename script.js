1230cf661a38da5c28d57d7f824dad5c85c5d1f9
PedroI <pedroigor2020@gmail.com>
2023-12-11 17:08:16 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+console.log("hello word");
+function imprimir(){
+    console.log("Conexão feita com sucesso!"); 
+    console.log("Conexão feita com sucesso!"); 
+    console.log("Conexão feita com sucesso!"); 
+}
+
+imprimir();
